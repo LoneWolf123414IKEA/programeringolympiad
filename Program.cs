a89f8e78b5545a3d6b69c4c28d47959dd19a6e2c
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-11-25 10:54:17 +0100

    unfinished shit

--- Program.cs
+++ Program.cs
@@ -5,41 +5,28 @@ namespace kap3upp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How log does it take tor to eat?");
-            int tor = int.Parse(Console.ReadLine());
-            Console.WriteLine("How log does it take mom to eat?");
-            int mom = int.Parse(Console.ReadLine());
-            int tors = 0;
-            int moms = 0;
-            int torc = 1;
-            int momc = 1;
-            int crrts = 0;
-            while (crrts<38)
+            string inp = Console.ReadLine();
+            int temp = 0;
+            int sto;
+            int cnt = 0;
+            int cnt1 = 0;
+            foreach(int i in inp)
             {
-                if ((tors == tor) && (moms == mom) && (crrts == 37))
+                sto = temp;
+                temp = i - 48;
+                if (sto == 1 || sto == 2)
                 {
-                    break;
+                    if (temp == 1 || temp == 2)
+                    {
+                        cnt1++;
+                    }
+                    else
+                    {
+                        cnt++;
+                    }
                 }
-                else if (tor == tors)
-                {
-                    tors = 0;
-                    torc++;
-                    crrts++;
-                }
-                if ((tors == tor) && (moms == mom) && (crrts == 39))
-                {
-                    break;
-                }
-                else if (mom == moms)
-                {
-                    moms = 0;
-                    momc++;
-                    crrts++;
-                }
-                moms++;
-                tors++;
             }
-            Console.WriteLine("tor ate " + torc + " carrots and mom ate " + momc + " carrots!");
+            Console.WriteLine(cnt + " " + cnt1);
         }
     }
 }
